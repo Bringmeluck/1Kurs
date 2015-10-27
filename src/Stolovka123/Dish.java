ea51e5c8f4ccb1c883202908a173f85d5ad74ed4
Bringmeluck <bringhorizon69@mail.ru>
2015-10-27 20:54:08 +0200

    Initial commit

---
+++ src/Stolovka123/Dish.java
@@ -0,0 +1,15 @@
+package Stolovka123;
+
+/**
+ * Created by user on 27.10.2015.
+ */
+public class Dish {
+    int prices;
+    String name;
+    int calorie;
+    public Dish(int prices, String name, int calorie){
+        this.prices = prices;
+        this.name = name;
+        this.calorie = calorie;
+    }
+}
